aa17fe977ea24d745f13aa497ace03fd36094cae
Tomas Wiklo <kaizen@Tomass-MacBook-Pro.local>
2020-05-08 15:41:04 +0200

    initial commit

---
+++ assets/js/realEstate/realEstate.js
@@ -0,0 +1,21 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  clickedBtn = () => {
+    console.log('swag')
+  }
+  render () {
+    return (<div>Real Estate</div>)
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<App />, app)
